f66e26b5142e1c4d308a96b762bd2008689d3bd9
Stefan Heigl <stefan.heigl90@gmail.com>
2024-01-14 22:06:29 +0100

    fix missing configService inejction

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const CronJob = require('cron').CronJob;
 const configService = require('./service/config-service')
 const forecastService = require('./service/forecast-service');
 
+const config = configService.getConfig();
 var job = new CronJob(
     config.cron,
     async () => await forecastService.forecast(),
